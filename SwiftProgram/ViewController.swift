7fa4b11e32d937015ff6357fce7ace7378b9a042
jeffy7 <jeffy7>
2016-03-29 10:58:04 +0800

    Initial Commit

---
+++ SwiftProgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftProgram
+//
+//  Created by je_ffy on 16/3/29.
+//  Copyright © 2016年 je_ffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
